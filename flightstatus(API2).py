e72f1eb8eaab00fa4e5b4ca82d96b51175ffc9a4
melissaw7 <99514180+melissaw7@users.noreply.github.com>
2023-11-29 19:08:53 -0500

    Update Skyscanner file

---
+++ flightstatus(API2).py
@@ -0,0 +1,36 @@
+import requests
+import json
+import unittest
+import os
+import math
+import matplotlib.pyplot as plt
+
+def read_data_from_file(filename):
+    """
+    Reads data from a file with the given filename.
+
+    Parameters
+    -----------------------
+    filename: str
+        The name of the file to read.
+
+    Returns
+    -----------------------
+    dict:
+        Parsed JSON data from the file.
+    """
+
+    url = ('https://api.traveltimeapp.com/v4/time-filter'
+        'apiKey=8dd3b29c0e475934424a9f54446aee3f''appId=9955bc4d')
+    response = requests.get(url)
+    print(response.json())
+
+    full_path = os.path.join(os.path.dirname(__file__), filename)
+    f = open(full_path)
+    file_data = f.read()
+    f.close()
+    json_data = json.loads(file_data)
+    return json_data
+
+def main():
+   pass
